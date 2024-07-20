3c88bec1f9957ee8cb6b2181c6725248b857fbbe
Inu <i0n4h0o7@gmail.com>
2024-07-20 12:38:41 +0900

    24.07.20 BottomNavigationBar 생성

--- app/src/main/java/com/inu/shoppingapp/MainActivity.java
+++ app/src/main/java/com/inu/shoppingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.inu.shoppingapp;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
     }
